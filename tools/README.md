383e27b4ce18e2c709162ba3fcb1c8911699c0be
xiyou <1693524836@qq.com>
2019-12-16 10:43:12 +0800

    三数之和

---
+++ tools/README.md
@@ -0,0 +1,9 @@
+## 6. 算法刷题
+
+|题目|解答  |
+|--|--|
+|[无重复最长子字符串的长度](https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/submissions/)  |  [我的代码](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/str/LengthOfLongestSubstring.java)|
+[最长回文字符串](https://leetcode-cn.com/problems/longest-palindromic-substring/)| [github代码](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/str/LongestPalindrome.java)  | 
+|[Z字形变换](https://leetcode-cn.com/problems/zigzag-conversion/)|[github代码](https://github.com/HouChenggong/springboot_dubbo/commit/2a17b3de081d26386e0160ae0a5a6d6635d74658)|
+|[数组最长前缀字符串](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/list/ZuiChang.java)||
+|[三数之和](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/list/SanShu.java)||
--- tools/src/main/java/cn/net/health/tools/list/SanShu.java
+++ tools/src/main/java/cn/net/health/tools/list/SanShu.java
@@ -1,9 +1,62 @@
 package cn.net.health.tools.list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author xiyou
  * @version 1.2
  * @date 2019/12/16 9:29
  */
 public class SanShu {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList();
+        int len = nums.length;
+        if (nums == null || len < 3) {
+            return ans;
+        }
+        Arrays.sort(nums); // 排序
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // 去重
+            }
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) {
+                        L++; // 去重
+                    }
+                    while (L < R && nums[R] == nums[R - 1]) {
+                        R--; // 去重
+                    }
+                    L++;
+                    R--;
+                } else if (sum < 0) {
+                    L++;
+                } else if (sum > 0) {
+                    R--;
+                }
+            }
+        }
+        return ans;
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-1, 0, 1, 2, -1, 2, -4};
+        System.out.println(threeSum(arr));
+    }
+
+
 }
