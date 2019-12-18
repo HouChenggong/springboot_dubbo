02695c72b8c0848ec0520385d86c20905e494596
xiyou <1693524836@qq.com>
2019-12-18 10:40:12 +0800

    最接近的三数之和

--- tools/README.md
+++ tools/README.md
@@ -7,3 +7,4 @@
 |[Z字形变换](https://leetcode-cn.com/problems/zigzag-conversion/)|[github代码](https://github.com/HouChenggong/springboot_dubbo/commit/2a17b3de081d26386e0160ae0a5a6d6635d74658)|
 |[数组最长前缀字符串](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/list/ZuiChang.java)||
 |[三数之和](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/list/SanShu.java)||
+|[最接近的三数之和](https://github.com/HouChenggong/springboot_dubbo/blob/master/tools/src/main/java/cn/net/health/tools/list/SanShu.java)||
--- tools/src/main/java/cn/net/health/tools/list/SanShu.java
+++ tools/src/main/java/cn/net/health/tools/list/SanShu.java
@@ -51,11 +51,62 @@ public class SanShu {
     }
 
 
+    /**
+     * 力扣16. 最接近的三数之和
+     * https://leetcode-cn.com/problems/3sum-closest/
+     * 击败63.64%的用户
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int threeSum(int[] nums, Integer target) {
+
+        int len = nums.length;
+        if (nums == null || len < 3) {
+            return 0;
+        }
+        Arrays.sort(nums); // 排序
+        int cha = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < len; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // 去重
+            }
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+
+                if (Math.abs(sum - target) < Math.abs(cha - target)) {
+                    cha = sum;
+                }
+                if (sum < target) {
+                    L++;
+                    while (L < R - 2 && nums[L] == nums[L + 1]) {
+                        L++; // 去重，但是不能去的太过分，一定要判断是否L<R-2
+                    }
+                } else if (sum > target) {
+                    R--;
+                    while (L < R - 2 && nums[R] == nums[R - 1]) {
+                        R--; // 去重
+                    }
+                } else {
+                    return target;
+                }
+            }
+        }
+        return cha;
+
+    }
 
 
     public static void main(String[] args) {
         int[] arr = new int[]{-1, 0, 1, 2, -1, 2, -4};
+        int[] arr2 = new int[]{-1, 0, 1, 1, 55};
+
         System.out.println(threeSum(arr));
+        System.out.println(threeSum(arr2, 1));
     }
 
 
