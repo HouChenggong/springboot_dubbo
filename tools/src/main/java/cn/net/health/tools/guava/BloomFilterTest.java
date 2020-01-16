4a15de324840c800e1b369315223cc4f9e3f7461
xiyou <1693524836@qq.com>
2020-01-16 09:46:40 +0800

    测试布隆过滤器

---
+++ tools/src/main/java/cn/net/health/tools/guava/BloomFilterTest.java
@@ -0,0 +1,23 @@
+package cn.net.health.tools.guava;
+
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.nio.charset.Charset;
+
+public class BloomFilterTest {
+
+    public static void main(String[] args) {
+        BloomFilter<String> stringBloomFilter =
+                BloomFilter.create(Funnels.stringFunnel(Charset.forName("utf-8")), 1000, 0.001);
+        stringBloomFilter.put("a");
+        stringBloomFilter.put("a2");
+        stringBloomFilter.put("a2");
+        stringBloomFilter.put("a3");
+        stringBloomFilter.put("a4");
+        stringBloomFilter.put("a5");
+        System.out.println(stringBloomFilter.mightContain("a6"));
+        System.out.println(stringBloomFilter.mightContain("a"));
+    }
+}
