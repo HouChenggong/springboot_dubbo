a62785231cf4e125380bf4fb034007012c2c26b0
yanxiyou <xiyouyan@outlook.com>
2020-03-11 18:56:26 +0800

    计算机网络相关

--- tools/src/main/java/cn/net/health/tools/lock/A.java
+++ tools/src/main/java/cn/net/health/tools/lock/A.java
@@ -14,26 +14,46 @@ public class A {
 
 
     }
+
     public static int maxProfit3(int[] prices) {
         int len = prices.length;
         if (len <= 1) {
             return 0;
         }
-        int low = prices[0];
-        int max = 0;
+        //max0存最大值，max1存第二大
+        int max[] = new int[]{0, 0};
+        int num = 1;
         for (int i = 1; i < len; i++) {
-            if (prices[i] > low) {
-                max = Math.max(max, prices[i] - low);
+            if (prices[i] > prices[i]) {
+                if (num == 1) {
+                    //直接上升
+                    max[0] += prices[i] - prices[i - 1];
+                    num = 1;
+                } else {
+                    //先下降后上升
+                    max[1] += prices[i] - prices[i - 1];
+                    num = 0;
+                }
+                if (max[1] > 0) {
+                    //说明有两次了
+                    if (max[0] < max[1]) {
+                        //如果第一个不是最大值，交换
+                        int temp = max[1];
+                        max[1] = max[0];
+                        max[0] = temp;
+                    }
+
+                }
             } else {
-                low = prices[i];
+                num = 0;
             }
         }
 
 
- 
-        return max;
+        return max[0];
 
     }
+
     public static int maxProfit(int[] prices) {
         int len = prices.length;
         if (len <= 1) {
@@ -51,23 +71,25 @@ public class A {
         return max;
 
     }
-    public static  int maxProfit2(int[] prices) {
+
+    public static int maxProfit2(int[] prices) {
         int len = prices.length;
         if (len <= 1) {
             return 0;
         }
 
-        int low=0;
+        int low = 0;
         int max = 0;
         for (int i = 1; i < len; i++) {
-            if (prices[i] > prices[i-1]) {
-                max +=prices[i]-prices[i-1];
+            if (prices[i] > prices[i - 1]) {
+                max += prices[i] - prices[i - 1];
             } else {
                 low = prices[i];
             }
         }
         return max;
     }
+
 
     public static void main(String[] args) {
         A a = new A();
